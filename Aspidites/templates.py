eb9e744141fef4af4148a38527f528315cb3936a
formica <42073607+rjdbcm@users.noreply.github.com>
2021-07-03 00:12:36 -0500

    templates.py cleanup

--- Aspidites/templates.py
+++ Aspidites/templates.py
@@ -22,15 +22,14 @@ setup(
 env = Template("""# cython language_level=3
 # THIS FILE IS GENERATED - DO NOT EDIT #
 import cython
-from Aspidites.features import *
-from Aspidites.monads import Maybe, Surely, Undefined
-from Aspidites.features.contracts import contract, new_contract
 from pyrsistent import pset
 from pyrsistent import pmap
 from pyrsistent import pvector
 from pyrsistent import PRecord
 from pyrsistent import PClass
-
+from Aspidites.features import *
+from Aspidites.monads import Maybe, Surely, Undefined
+from Aspidites.features.contracts import contract, new_contract
 from Aspidites.features.RestrictedPython import safe_globals
 safe_globals.update(globals()) # add all imports to globals
 globals().update(safe_globals)
