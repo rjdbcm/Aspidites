ada0f5fff3b9c3b19b9c6394217bd3097dac6de9
formica <42073607+rjdbcm@users.noreply.github.com>
2021-08-05 15:57:20 -0500

    Finished LICENSE info for all vendored packages.

--- Aspidites/_vendor/_compat.py
+++ Aspidites/_vendor/_compat.py
@@ -1,3 +1,17 @@
+##############################################################################
+# Modified to work as a lightweight alternative to vendoring six.
+# Originally from the package RestrictedPython.
+# Copyright (c) 2002 Zope Foundation and Contributors.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
 import platform
 import sys
 
