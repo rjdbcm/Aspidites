807690a176bae4d8b590c0c809702fa9c17bff71
formica <42073607+rjdbcm@users.noreply.github.com>
2021-08-16 18:04:27 -0500

    Added welcome message to help. Fixed test_aspidites.py paths

--- Aspidites/__init__.py
+++ Aspidites/__init__.py
@@ -1,2 +1,13 @@
 __version__ = '0.8.0'
 __title__ = 'Aspidites'
+__author__ = 'Ross J. Duff'
+__license__ = "GPL"
+__description__ = (
+    f"""
+    {__title__} v{__version__} is the reference Woma programming language compiler.
+    Copyright (C) 2021  {__author__}
+    This program comes with ABSOLUTELY NO WARRANTY; 
+    This is free software, and you are welcome to redistribute it
+    under the conditions of the {__license__}v3 found in the LICENSE file.
+    """
+)
