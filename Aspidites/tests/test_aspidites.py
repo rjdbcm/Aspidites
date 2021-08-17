026d8b70e59a181cc6dedc5b663f8198dbe53ce4
formica <42073607+rjdbcm@users.noreply.github.com>
2021-08-17 00:10:21 -0500

    tests now run from Aspidites -pt/--pytest Or they can be discovered from the project directory.

--- Aspidites/tests/test_aspidites.py
+++ Aspidites/tests/test_aspidites.py
@@ -1,4 +1,5 @@
 import os
+import warnings
 
 from Aspidites._vendor.contracts import ContractNotRespected
 from hypothesis import given, assume, strategies as st
@@ -8,17 +9,35 @@ from Aspidites.parser import parse_module
 from Aspidites.templates import lib, setup
 from Aspidites.monads import Maybe, Undefined, Surely
 from Aspidites.compiler import compile_module
+
 docker = os.getenv("ASPIDITES_DOCKER_BUILD")
 
-wfile = ('examples' if docker else 'Aspidites/tests/examples') + '/examples.wom'
-pfile = ('examples' if docker else 'Aspidites/tests/examples') + '/compiled.py'
-
-with open(wfile, 'r') as f:
-    code_ = parse_module(f.read())
+wfile = 'examples/examples.wom'
 
 
-def test_compile_module():
-    compile(lib.substitute(code='\n'.join(code_)), '', 'exec')
+@pt.fixture(autouse=True)
+def inject_config(request):
+    return request.config.rootpath
+
+
+def setup_code(inject_config):
+    if os.path.exists(wfile):
+        code_ = parse_module(open(wfile, 'r').read())
+    else:
+        try:
+            code_ = parse_module(open("Aspidites/tests/" + wfile, 'r').read())
+            warnings.warn("Aspidites is being tested in source mode")
+        except FileNotFoundError:
+            code_ = parse_module(open(os.path.join(inject_config, wfile), 'r').read())
+    return code_
+
+
+def test_compile_module(inject_config):
+    try:
+        compile(lib.substitute(code='\n'.join(setup_code(inject_config))), '', 'exec')
+    except FileNotFoundError:
+        compile(lib.substitute(code='\n'.join(os.path.join(inject_config(), wfile))), '',
+                'exec')
 
 
 @given(x=st.integers())
@@ -56,11 +75,17 @@ def test_integer_monad(x):
     assert oct(Surely(x)) == oct(x)
 
 
-def test_compile_to_shared_object():
+def test_compile_to_shared_object(inject_config):
+    pfile_ = 'examples/compiled.py'
+    pfile = pfile_ if os.path.exists(wfile) else 'Aspidites/tests/' + pfile_
+    try:
+        compile_module(setup_code(inject_config), pfile, bytecode=True, **get_cy_kwargs())
+    except FileNotFoundError:
+        compile_module(setup_code(inject_config), os.path.join(inject_config, pfile_),
+                       bytecode=True, **get_cy_kwargs())
 
-    compile_module(code_, pfile, bytecode=True, **get_cy_kwargs())
-
-    from Aspidites.tests.examples.compiled import Add, x, y, z, scala, val, div_by_zero, Yield123, Hello, Hello2
+    from Aspidites.tests.examples.compiled import Add, x, y, z, scala, val, div_by_zero, \
+        Yield123, Hello, Hello2
 
     with pt.raises(ContractNotRespected):
         Add(x=6.5, y=12)
@@ -74,7 +99,6 @@ def test_compile_to_shared_object():
     assert val() == Undefined()
     assert div_by_zero() == Undefined()
     Hello()
-
 
 #
 # def teardown_function():
