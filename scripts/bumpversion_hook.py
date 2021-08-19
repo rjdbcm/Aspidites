d2940d4007710e3576e2e8afa4a263ac758b0cc4
formica <42073607+rjdbcm@users.noreply.github.com>
2021-08-19 18:46:31 -0500

    added version bump and changelog builder

---
+++ scripts/bumpversion_hook.py
@@ -0,0 +1,27 @@
+import os
+
+from Aspidites import __version__
+
+
+def main():
+    vstring = f'**v{__version__}**'
+    # Read in the file
+    data = open('CHANGELOG.md', 'r').read()
+    open('CHANGELOG.bak', 'w').write(data)
+
+    # Replace the target string
+    commit_log = '\n- '.join(
+        os.popen(f'git log --abbrev-commit --pretty=oneline {vstring}..HEAD').readlines()
+    )
+    changes = '\n'.join(
+        [vstring, commit_log, '\n']
+    )
+
+    data = data.replace(vstring, changes)
+
+    # Write the file out again
+    open('CHANGELOG.md', 'w').write(data)
+
+
+if __name__ == '__main__':
+    main()
