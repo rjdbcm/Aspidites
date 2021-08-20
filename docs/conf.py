79440bae306aa933c3880711f19405f76c154ce0
formica <42073607+rjdbcm@users.noreply.github.com>
2021-08-19 23:04:35 -0500

    update docs logo

--- docs/conf.py
+++ docs/conf.py
@@ -65,8 +65,8 @@ html_theme_options.update(display_version=True,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_favicon = '_static/aspidites_logo_96_96.png'
-html_logo = '_static/aspidites_logo_96_96.png'
+html_favicon = '_static/aspidites_logo_wheelie_96_96.png'
+html_logo = '_static/aspidites_logo_wheelie_96_96.png'
 
 
 class WomaLexer(RegexLexer):
