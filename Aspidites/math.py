d8ff880cb455b3c9c258a9f153e9fa4d6b3dd8e4
Ross J. Duff <rjdbcm@mail.umkc.edu>
2021-10-12 01:03:18 -0500

    improve readability

--- Aspidites/math.py
+++ Aspidites/math.py
@@ -1,16 +1,18 @@
-#cython: language_level=3, annotation_typing=True, c_string_encoding=utf-8
+# cython: language_level=3, annotation_typing=True, c_string_encoding=utf-8
 import sys
 from warnings import warn
-from typing import Any, Union, TypeVar, ItemsView, Type
-from inspect import isfunction, signature, getouterframes
+from typing import Any, Union, ItemsView, NoReturn
+from inspect import getouterframes
 from cmath import inf, isinf, nan, isnan
 import numbers
 import cython
 import numpy as np
+from inspect import isfunction, signature
 from ._vendor.fn.underscore import _Callable
 from pyrsistent import v, pvector, PVector
 from .templates import _warning
 from .api import bordered
+
 Numeric = Union[int, float, complex, np.number]
 
 
@@ -103,7 +105,7 @@ def SafeUnaryAdd(a: Numeric) -> Union[Numeric, Undefined]:
     exc: Exception
     if isnan(a) or not isinstance(a, numbers.Number):
         stack = pvector(getouterframes(sys._getframe(0), 1))
-        exc = ZeroDivisionError(f"Unary Add is Undefined for {type(a)}")
+        exc = ArithmeticError(f"Unary Add is Undefined for {type(a)}")
         w = Warn(stack, stack[0][3], [a], {}).create(exc)
         warn(w, category=RuntimeWarning, stacklevel=0)
         return Undefined(SafeUnaryAdd, a)
@@ -118,7 +120,7 @@ def SafeUnarySub(a: Numeric) -> Union[Numeric, Undefined]:
     exc: Exception
     if isnan(a) or not isinstance(a, numbers.Number):
         stack = pvector(getouterframes(sys._getframe(0), 1))
-        exc = ZeroDivisionError(f"Unary Sub is Undefined for {type(a)}")
+        exc = ArithmeticError(f"Unary Sub is Undefined for {type(a)}")
         w = Warn(stack, stack[0][3], [a], {}).create(exc)
         warn(w, category=RuntimeWarning, stacklevel=0)
         return Undefined(SafeUnarySub, a)
@@ -135,7 +137,7 @@ def SafeFloorDiv(a: Numeric, b: Numeric) -> Union[Numeric, Undefined]:
     exc: Exception
     if isinf(a) or b == 0 or (isinf(a) and isinf(b)):
         stack = pvector(getouterframes(sys._getframe(0), 1))
-        exc = ZeroDivisionError("Division by zero is Undefined; this behavior diverges from IEEE 754-1985.")
+        exc = ArithmeticError("Division by zero is Undefined; this behavior diverges from IEEE 754-1985.")
         w = Warn(stack, stack[0][3], [a, b], {}).create(exc)
         warn(w, category=RuntimeWarning, stacklevel=0)
         return Undefined(SafeFloorDiv, a, b)
@@ -152,7 +154,7 @@ def SafeDiv(a: Numeric, b: Numeric) -> Union[Numeric, Undefined]:
     exc: Exception
     if b == 0 or (isinf(a) and isinf(b)):
         stack = pvector(getouterframes(sys._getframe(0), 1))
-        exc = ZeroDivisionError("Division by zero is Undefined; this behavior diverges from IEEE 754-1985.")
+        exc = ArithmeticError("Division by zero is Undefined; this behavior diverges from IEEE 754-1985.")
         w = Warn(stack, stack[0][3], [a, b], {}).create(exc)
         warn(w, category=RuntimeWarning, stacklevel=0)
         return Undefined(SafeDiv, a, b)
@@ -169,7 +171,7 @@ def SafeMod(a: Numeric, b: Numeric) -> Union[Numeric, Undefined]:
     exc: Exception
     if isinf(a) or b == 0:
         stack = pvector(getouterframes(sys._getframe(0), 1))
-        exc = ZeroDivisionError("Modulus by zero is Undefined; this behavior diverges from IEEE 754-1985.")
+        exc = ArithmeticError("Modulus by zero is Undefined; this behavior diverges from IEEE 754-1985.")
         w = Warn(stack, stack[0][3], [a, b], {}).create(exc)
         warn(w, category=RuntimeWarning, stacklevel=0)
         return Undefined(SafeMod, a, b)
@@ -186,27 +188,30 @@ def SafeExp(a: Numeric, b: Numeric) -> Union[Numeric, Undefined]:
     # 0**0, inf**0, 0**inf
     if (a == 0 and b == 0) or (isinf(a) and b == 0) or (isinf(b) and a == 0):  # pragma: no cover
         stack = pvector(getouterframes(sys._getframe(0), 1))
-        exc = ArithmeticError(
-            f"{str(a)}**{str(b)} == Undefined; this behavior diverges from IEEE 754-1985."
-        )
+        exc = ArithmeticError(f"{str(a)}**{str(b)} == Undefined; this behavior diverges from IEEE 754-1985.")
         w = Warn(stack, stack[0][3], [a, b], {}).create(exc)
         warn(w, category=RuntimeWarning, stacklevel=0)
         return Undefined(SafeExp, a, b)
     try:
-        return a**b
+        return a ** b
     except OverflowError:
         return inf  # just a really big number on most systems
 
 
-def _format_locals(lokals, str_locals):
-    for k, v_ in lokals.items():
-        if str(k).startswith("@"):  # skip @py_assert
-            continue
-        if isfunction(v_):
-            s = str(signature(v_)).replace("'", "")
+def _format_locals(lokals: dict):
+    d = {k: v for k, v_ in lokals.items() if not str(k).startswith('@')}
+    str_locals = _format_items(d)
+    return str_locals
+
+
+def _format_items(x):
+    str_locals = ''.encode('UTF-8')
+    for k, v in x.items():
+        if isfunction(v):
+            s = str(signature(v)).replace("'", "")
             str_locals += f"{k}: {s}\n".encode('UTF-8')
         else:
-            str_locals += f"{k}: {str(v_)}\n".encode('UTF-8')
+            str_locals += f"{k}: {str(v)}\n".encode('UTF-8')
     return str_locals
 
 
@@ -228,7 +233,7 @@ class Warn:
         self.args = args
         self.kwargs = kwargs
 
-    def create(self, exc=Exception()):
+    def create(self, exc=Exception()) -> str:
         local_items = self.stack[1][0].f_locals.items()
         str_locals = self.format_locals(local_items, exc)
         func_name = self.stack[1][0].f_code.co_name
@@ -247,11 +252,11 @@ class Warn:
             tb=bordered(str(exc)),
         )
 
-    def format_kwargs(self, sep: str = ", "):
+    def format_kwargs(self, sep: str = ", ") -> bytes:
         return f'{sep}{str(self.kwargs).strip("{} ").replace(":", "=") if len(self.kwargs) else ""}'.encode('UTF-8')
 
     # noinspection PyMethodMayBeStatic
-    def format_locals(self, local_vars, exc: Exception):
-        locals_: dict = dict(filter(lambda x: x[1] != str(exc), local_vars))
-        str_locals: bytes = _format_locals(locals_, ''.encode('UTF-8'))
+    def format_locals(self, local_vars, exc: Exception) -> bytes:
+        lokals: dict = dict(filter(lambda x: x[1] != str(exc), local_vars))
+        str_locals: bytes = _format_locals(lokals)
         return str_locals.rstrip("\n".encode('UTF-8'))
