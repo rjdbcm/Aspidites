b0486b2e1ccdbef4943fd14d6d09f1dd1a199252
Ross J. Duff, MSc <42073607+rjdbcm@users.noreply.github.com>
2021-10-15 11:50:56 -0500

    Revert "changed line:4019 parseImpl to prebind string length"
    This reverts commit 3d2f61356ef3b8f8a7268f56cc90f72bced52244.

--- Aspidites/_vendor/pyparsing_extension.py
+++ Aspidites/_vendor/pyparsing_extension.py
@@ -4019,29 +4019,18 @@ class MatchFirst(ParseExpression):
     def parseImpl(self, instring, loc, doActions=True):
         maxExcLoc = -1
         maxException = None
-        instringlen: int = len(instring)
         for e in self.exprs:
             try:
                 ret = e._parse(instring, loc, doActions)
                 return ret
-            except (ParseException, ValueError) as err:
-                if type(err) == ParseException:
-                    if err.loc > maxExcLoc:
-                        maxException = err
-                        maxExcLoc = err.loc
-                if type(err) == ValueError:
-                    if instringlen > maxExcLoc:
-                        maxException = ParseException(instring, instringlen, e.errmsg, self)
-                        maxExcLoc = instringlen
-
-            # except ParseException as err:
-            #     if err.loc > maxExcLoc:
-            #         maxException = err
-            #         maxExcLoc = err.loc
-            # except IndexError:
-            #     if len(instring) > maxExcLoc:
-            #         maxException = ParseException(instring, len(instring), e.errmsg, self)
-            #         maxExcLoc = len(instring)
+            except ParseException as err:
+                if err.loc > maxExcLoc:
+                    maxException = err
+                    maxExcLoc = err.loc
+            except IndexError:
+                if len(instring) > maxExcLoc:
+                    maxException = ParseException(instring, len(instring), e.errmsg, self)
+                    maxExcLoc = len(instring)
 
         # only got here if no expression matched, raise exception for match that made it the furthest
         else:
