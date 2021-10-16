697a27126ce1354789e69987127de0b4ed86d566
Ross J. Duff, MSc <42073607+rjdbcm@users.noreply.github.com>
2021-10-15 21:14:27 -0500

    added inspection.py unit tests

--- Aspidites/tests/test_importlib/util.py
+++ Aspidites/tests/test_importlib/util.py
@@ -6,8 +6,7 @@ from importlib import machinery, util, invalidate_caches
 import marshal
 import os
 import os.path
-from test.support import import_helper
-from test.support import os_helper
+from Aspidites.tests.support import import_helper
 import unittest
 import sys
 import tempfile
@@ -148,28 +147,28 @@ def uncache(*names):
                 pass
 
 
-@contextlib.contextmanager
-def temp_module(name, content='', *, pkg=False):
-    conflicts = [n for n in sys.modules if n.partition('.')[0] == name]
-    with os_helper.temp_cwd(None) as cwd:
-        with uncache(name, *conflicts):
-            with import_helper.DirsOnSysPath(cwd):
-                invalidate_caches()
-
-                location = os.path.join(cwd, name)
-                if pkg:
-                    modpath = os.path.join(location, '__init__.py')
-                    os.mkdir(name)
-                else:
-                    modpath = location + '.py'
-                    if content is None:
-                        # Make sure the module file gets created.
-                        content = ''
-                if content is not None:
-                    # not a namespace package
-                    with open(modpath, 'w', encoding='utf-8') as modfile:
-                        modfile.write(content)
-                yield location
+# @contextlib.contextmanager
+# def temp_module(name, content='', *, pkg=False):
+#     conflicts = [n for n in sys.modules if n.partition('.')[0] == name]
+#     with os_helper.temp_cwd(None) as cwd:
+#         with uncache(name, *conflicts):
+#             with import_helper.DirsOnSysPath(cwd):
+#                 invalidate_caches()
+#
+#                 location = os.path.join(cwd, name)
+#                 if pkg:
+#                     modpath = os.path.join(location, '__init__.py')
+#                     os.mkdir(name)
+#                 else:
+#                     modpath = location + '.py'
+#                     if content is None:
+#                         # Make sure the module file gets created.
+#                         content = ''
+#                 if content is not None:
+#                     # not a namespace package
+#                     with open(modpath, 'w', encoding='utf-8') as modfile:
+#                         modfile.write(content)
+#                 yield location
 
 
 @contextlib.contextmanager
@@ -333,63 +332,63 @@ def temporary_pycache_prefix(prefix):
     finally:
         sys.pycache_prefix = _orig_prefix
 
-
-@contextlib.contextmanager
-def create_modules(*names):
-    """Temporarily create each named module with an attribute (named 'attr')
-    that contains the name passed into the context manager that caused the
-    creation of the module.
-
-    All files are created in a temporary directory returned by
-    tempfile.mkdtemp(). This directory is inserted at the beginning of
-    sys.path. When the context manager exits all created files (source and
-    bytecode) are explicitly deleted.
-
-    No magic is performed when creating packages! This means that if you create
-    a module within a package you must also create the package's __init__ as
-    well.
-
-    """
-    source = 'attr = {0!r}'
-    created_paths = []
-    mapping = {}
-    state_manager = None
-    uncache_manager = None
-    try:
-        temp_dir = tempfile.mkdtemp()
-        mapping['.root'] = temp_dir
-        import_names = set()
-        for name in names:
-            if not name.endswith('__init__'):
-                import_name = name
-            else:
-                import_name = name[:-len('.__init__')]
-            import_names.add(import_name)
-            if import_name in sys.modules:
-                del sys.modules[import_name]
-            name_parts = name.split('.')
-            file_path = temp_dir
-            for directory in name_parts[:-1]:
-                file_path = os.path.join(file_path, directory)
-                if not os.path.exists(file_path):
-                    os.mkdir(file_path)
-                    created_paths.append(file_path)
-            file_path = os.path.join(file_path, name_parts[-1] + '.py')
-            with open(file_path, 'w', encoding='utf-8') as file:
-                file.write(source.format(name))
-            created_paths.append(file_path)
-            mapping[name] = file_path
-        uncache_manager = uncache(*import_names)
-        uncache_manager.__enter__()
-        state_manager = import_state(path=[temp_dir])
-        state_manager.__enter__()
-        yield mapping
-    finally:
-        if state_manager is not None:
-            state_manager.__exit__(None, None, None)
-        if uncache_manager is not None:
-            uncache_manager.__exit__(None, None, None)
-        os_helper.rmtree(temp_dir)
+#
+# @contextlib.contextmanager
+# def create_modules(*names):
+#     """Temporarily create each named module with an attribute (named 'attr')
+#     that contains the name passed into the context manager that caused the
+#     creation of the module.
+#
+#     All files are created in a temporary directory returned by
+#     tempfile.mkdtemp(). This directory is inserted at the beginning of
+#     sys.path. When the context manager exits all created files (source and
+#     bytecode) are explicitly deleted.
+#
+#     No magic is performed when creating packages! This means that if you create
+#     a module within a package you must also create the package's __init__ as
+#     well.
+#
+#     """
+#     source = 'attr = {0!r}'
+#     created_paths = []
+#     mapping = {}
+#     state_manager = None
+#     uncache_manager = None
+#     try:
+#         temp_dir = tempfile.mkdtemp()
+#         mapping['.root'] = temp_dir
+#         import_names = set()
+#         for name in names:
+#             if not name.endswith('__init__'):
+#                 import_name = name
+#             else:
+#                 import_name = name[:-len('.__init__')]
+#             import_names.add(import_name)
+#             if import_name in sys.modules:
+#                 del sys.modules[import_name]
+#             name_parts = name.split('.')
+#             file_path = temp_dir
+#             for directory in name_parts[:-1]:
+#                 file_path = os.path.join(file_path, directory)
+#                 if not os.path.exists(file_path):
+#                     os.mkdir(file_path)
+#                     created_paths.append(file_path)
+#             file_path = os.path.join(file_path, name_parts[-1] + '.py')
+#             with open(file_path, 'w', encoding='utf-8') as file:
+#                 file.write(source.format(name))
+#             created_paths.append(file_path)
+#             mapping[name] = file_path
+#         uncache_manager = uncache(*import_names)
+#         uncache_manager.__enter__()
+#         state_manager = import_state(path=[temp_dir])
+#         state_manager.__enter__()
+#         yield mapping
+#     finally:
+#         if state_manager is not None:
+#             state_manager.__exit__(None, None, None)
+#         if uncache_manager is not None:
+#             uncache_manager.__exit__(None, None, None)
+#         os_helper.rmtree(temp_dir)
 
 
 def mock_path_hook(*entries, importer):
