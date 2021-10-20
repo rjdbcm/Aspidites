3660dbb736a4b343edea23341e8bceac1e1dd193
Ross J. Duff, MSc <42073607+rjdbcm@users.noreply.github.com>
2021-10-19 22:51:42 -0500

    yeeted dearpygui for the time being

--- Aspidites/woma/guiutils.py
+++ Aspidites/woma/guiutils.py
@@ -1,7 +1,10 @@
-import dearpygui.dearpygui as dpg
-
-import sys
-this = sys.modules[__name__]
-
-for i in dir(dpg):
-    setattr(this, i, getattr(dpg, i))
+"""Disabled as of 2.1.6 but it does seem to work.Except with CD
+"""
+# TODO Fix dearpygui module not found
+# import dearpygui.dearpygui as dpg
+#
+# import sys
+# this = sys.modules[__name__]
+#
+# for i in dir(dpg):
+#     setattr(this, i, getattr(dpg, i))
