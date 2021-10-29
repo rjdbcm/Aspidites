52cb3cb02dedd36ffac6097ff745a46026a90372
Ross J. Duff, MSc <42073607+rjdbcm@users.noreply.github.com>
2021-10-29 18:04:14 -0500

    v1.11.2 released

--- Aspidites/woma/__init__.py
+++ Aspidites/woma/__init__.py
@@ -24,7 +24,7 @@ from .mathutils import clamp, ceil, floor
 from contextlib import contextmanager
 from functools import lru_cache
 
-__version__ = '1.11.1'
+__version__ = '1.11.2'
 
 
 def woma_version():
