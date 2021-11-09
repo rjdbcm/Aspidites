6d567bc8eaa34adb24e7490dce8e9e5767186dee
Ross J. Duff, MSc <42073607+rjdbcm@users.noreply.github.com>
2021-11-09 16:42:03 -0600

    added safer type implementation for dealing with classes

--- Aspidites/type_guard.py
+++ Aspidites/type_guard.py
@@ -2,14 +2,16 @@ from _vendor.pyrsistent import PClass
 
 
 def safer_type(*args):
-    if len(args) < 3:
-        raise RuntimeError("Type introspection is not supported.")
-
     name: str
     bases: tuple
     attributes: dict
-
-    name, bases, attributes = args
-    bases = (PClass,) + bases
+    if len(args) < 2:
+        raise RuntimeError("Type introspection is not supported.")
+    elif len(args) == 2:
+        name, attributes = args
+        bases = (PClass,)
+    else:
+        name, bases, attributes = args
+        bases = (PClass,) + bases
 
     return type(name, bases, attributes)
