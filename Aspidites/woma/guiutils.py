0ecd2b48481b3fcdd86e9ee0726f0ed9da49a649
Ross J. Duff, MSc <42073607+rjdbcm@users.noreply.github.com>
2021-11-16 12:43:21 -0600

    reenable dearpygui dependency

--- Aspidites/woma/guiutils.py
+++ Aspidites/woma/guiutils.py
@@ -1,11 +1,10 @@
 # cython: language_level=3, annotation_typing=True, c_string_encoding=utf-8, boundscheck=False, wraparound=False, initializedcheck=False
 """Disabled as of 2.1.6 but it does seem to work.Except with CD
 """
-# TODO Fix dearpygui module not found
-# import dearpygui.dearpygui as dpg
-#
-# import sys
-# this = sys.modules[__name__]
-#
-# for i in dir(dpg):
-#     setattr(this, i, getattr(dpg, i))
+import dearpygui.dearpygui as dpg
+
+import sys
+this = sys.modules[__name__]
+
+for i in dir(dpg):
+    setattr(this, i, getattr(dpg, i))
