99fc114085ca9c31ff945cc0bec7e930f0d93d15
rjdbcm <rjdbcm@mail.umkc.edu>
2021-11-22 04:40:43 -0600

    pyparsing _trim_arity made static at the cost of breaking most tests

--- Aspidites/_vendor/pyparsing_extension.py
+++ Aspidites/_vendor/pyparsing_extension.py
@@ -5,6 +5,7 @@ import itertools
 import pprint
 import re
 import string
+import traceback
 import sys
 import types
 import warnings
@@ -83,6 +84,114 @@ def _enable_all_warnings():  # pragma: no cover
 
 
 __diag__.enable_all_warnings = _enable_all_warnings
+# build list of single arg builtins, that can be used as parse actions
+singleArgBuiltins = [
+    sum,
+    len,
+    sorted,
+    reversed,
+    list,
+    tuple,
+    set,
+    any,
+    all,
+    min,
+    max,
+]
+
+# Only works on Python 3.x - nonlocal is toxic to Python 2 installs
+# ~ 'decorator to trim function calls to match the arity of the target'
+# ~ def _trim_arity(func, maxargs=3):
+# ~ if func in singleArgBuiltins:
+# ~ return lambda s,l,t: func(t)
+# ~ limit = 0
+# ~ foundArity = False
+# ~ def wrapper(*args):
+# ~ nonlocal limit,foundArity
+# ~ while 1:
+# ~ try:
+# ~ ret = func(*args[limit:])
+# ~ foundArity = True
+# ~ return ret
+# ~ except TypeError:
+# ~ if limit == maxargs or foundArity:
+# ~ raise
+# ~ limit += 1
+# ~ continue
+# ~ return wrapper
+
+# this version is Python 2.x-3.x cross-compatible
+"decorator to trim function calls to match the arity of the target"
+
+
+def _trim_arity(func, maxargs=2):
+    if func in singleArgBuiltins:
+        return lambda s, l, t: func(t)
+    limit = [0]
+    foundArity = [False]
+
+    # traceback return data structure changed in Py3.5 - normalize back to plain tuples
+    if system_version[:2] >= (3, 5):
+
+        def extract_stack(limit=0):
+            # special handling for Python 3.5.0 - extra deep call stack by 1
+            offset = -3 if system_version == (3, 5, 0) else -2
+            frame_summary = traceback.extract_stack(limit=-offset + limit - 1)[offset]
+            return [frame_summary[:2]]
+
+        def extract_tb(tb, limit=0):
+            frames = traceback.extract_tb(tb, limit=limit)
+            frame_summary = frames[-1]
+            return [frame_summary[:2]]
+
+    else:
+        extract_stack = traceback.extract_stack
+        extract_tb = traceback.extract_tb
+
+    # synthesize what would be returned by traceback.extract_stack at the call to
+    # user's parse action 'func', so that we don't incur call penalty at parse time
+
+    LINE_DIFF = 6
+    # IF ANY CODE CHANGES, EVEN JUST COMMENTS OR BLANK LINES, BETWEEN THE NEXT LINE AND
+    # THE CALL TO FUNC INSIDE WRAPPER, LINE_DIFF MUST BE MODIFIED!!!!
+    this_line = extract_stack(limit=2)[-1]
+    pa_call_line_synth = (this_line[0], this_line[1] + LINE_DIFF)
+
+    def wrapper(*args):
+        while 1:
+            try:
+                ret = func(*args[limit[0] :])
+                foundArity[0] = True
+                return ret
+            except TypeError:
+                # re-raise TypeErrors if they did not come from our arity testing
+                if foundArity[0]:
+                    raise
+                else:
+                    try:
+                        tb = sys.exc_info()[-1]
+                        if not extract_tb(tb, limit=2)[-1][:2] == pa_call_line_synth:
+                            raise
+                    finally:
+                        try:
+                            del tb
+                        except NameError:
+                            pass
+
+                if limit[0] <= maxargs:
+                    limit[0] += 1
+                    continue
+                raise
+
+    # copy func name to wrapper for sensible debug output
+    func_name = "<parse action>"
+    try:
+        func_name = getattr(func, "__name__", getattr(func, "__class__").__name__)
+    except Exception:
+        func_name = str(func)
+    wrapper.__name__ = func_name
+
+    return wrapper
 
 
 def _defaultStartDebugAction(instring, loc, expr):
